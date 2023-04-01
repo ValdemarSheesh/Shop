61e8c4904c55c8a6b864d4b0000e4cfa9fac7f31
Vladimir Shahnyuk <107857461+ValdemarSheesh@users.noreply.github.com>
2023-04-01 12:50:18 +0300

    refactoring

--- src/main/java/com/example/Shop/ShopApplication.java
+++ src/main/java/com/example/Shop/ShopApplication.java
@@ -2,13 +2,12 @@ package com.example.Shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
 
 }
