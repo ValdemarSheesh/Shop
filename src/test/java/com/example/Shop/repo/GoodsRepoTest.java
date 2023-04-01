90e6604fc9e0a2b9c5a4c03d86f88576964d23fb
Vladimir Shahnyuk <107857461+ValdemarSheesh@users.noreply.github.com>
2023-04-01 19:56:15 +0300

    add tests for goodsRepo

---
+++ src/test/java/com/example/Shop/repo/GoodsRepoTest.java
@@ -0,0 +1,63 @@
+package com.example.Shop.repo;
+
+import com.example.Shop.ShopApplication;
+import com.example.Shop.model.Goods;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@Transactional
+@SpringBootTest(classes = ShopApplication.class)
+public class GoodsRepoTest {
+
+    @Autowired
+    private GoodsRepository goodsRepository;
+
+    @Test
+    public void testFindById() {
+        Goods goods = getGoods();
+        goodsRepository.saveAndFlush(goods);
+        Goods result = goodsRepository.findById(goods.getId()).get();
+        assertEquals(goods.getId(), result.getId());
+    }
+
+    @Test
+    public void testSaveAndFlush() {
+        Goods goods = getGoods();
+        goodsRepository.saveAndFlush(goods);
+        Goods found = goodsRepository.findById(goods.getId()).get();
+        assertEquals(goods.getId(), found.getId());
+    }
+
+    @Test
+    public void testDeleteById() {
+        Goods goods = getGoods();
+        goodsRepository.saveAndFlush(goods);
+        goodsRepository.delete(goods);
+        List<Goods> result = goodsRepository.findAll();
+        assertEquals(result.size(), 0);
+    }
+
+    @Test
+    public void testFindAll() {
+        Goods goods = getGoods();
+        goodsRepository.saveAndFlush(goods);
+        List<Goods> result = goodsRepository.findAll();
+        assertEquals(result.size(), 1);
+    }
+
+    private Goods getGoods() {
+        Goods goods = new Goods();
+        goods.setName("nameGoods");
+        goods.setPrice(99.99);
+        return goods;
+    }
+}
